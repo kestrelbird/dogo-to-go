d1d92cd60ad33d4b35b7b7eb3f1f0b0a67940020
Monika Pustuła <moonika.pustua@gmail.com>
2024-02-26 12:08:57 +0100

    Initial commit
    create temporary index.html and main.css

---
+++ src/main/java/com/monikapustula/dogotogo/DogoToGoApplication.java
@@ -0,0 +1,13 @@
+package com.monikapustula.dogotogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogoToGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogoToGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monikapustula/dogotogo/DogoToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monikapustula.dogotogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogoToGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

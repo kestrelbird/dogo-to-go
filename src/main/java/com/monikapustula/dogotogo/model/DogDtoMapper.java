3fc5ba573d04d1e04d8220912383ae25f246b345
Monika Pustuła <moonika.pustua@gmail.com>
2024-02-29 11:46:14 +0100

    create architecture, update form

---
+++ src/main/java/com/monikapustula/dogotogo/model/DogDtoMapper.java
@@ -0,0 +1,4 @@
+package com.monikapustula.dogotogo.model;
+
+public class DogDtoMapper {
+}
--- src/main/java/com/monikapustula/dogotogo/web/FormController.java
+++ src/main/java/com/monikapustula/dogotogo/web/FormController.java
@@ -1,7 +1,10 @@
 package com.monikapustula.dogotogo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -9,5 +12,15 @@ public class FormController {
     @GetMapping("/form")
     String getForm() {
         return "form";
+    }
+
+    @PostMapping("/save")
+    String getOutcome(@RequestParam int size, @RequestParam int activity, @RequestParam int coatCare) {
+        return "redirect:outcome";
+    }
+
+    @GetMapping("/outcome")
+    String getOutcome() {
+        return "outcome";
     }
 }

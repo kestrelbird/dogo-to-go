663f054868f4d6c5a88ed740f7c6c6fc5b50c690
Monika Pustuła <moonika.pustua@gmail.com>
2024-04-11 11:29:50 +0200

    update files for prod profile

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build . -t dogotogo
 docker stop dogotogo || true
 docker rm dogotogo || true
-docker run -d -p 8080:8080 --name=dogotogo dogotogo
+docker run -d -p 8080:8080 --name=dogotogo -e SPRING_PROFILES_ACTIVE=prod --network dogo-network --restart unless-stopped dogotogo
